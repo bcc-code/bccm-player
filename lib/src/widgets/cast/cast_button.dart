4783b62418a9e3668cf23600a7987ac8737f8c30
Andreas Gangsø <andreasgangso@gmail.com>
2024-04-17 14:32:55 +0200

    fix: strange castbutton size layout error

--- lib/src/widgets/cast/cast_button.dart
+++ lib/src/widgets/cast/cast_button.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
 import 'package:universal_io/io.dart';
 
 import 'package:flutter/material.dart';
@@ -17,12 +20,7 @@ class CastButton extends StatelessWidget {
     if (Platform.isAndroid) {
       return SizedBox(
         width: 24,
-        child: AndroidView(
-          viewType: 'bccm_player/cast_button',
-          layoutDirection: TextDirection.ltr,
-          creationParams: creationParams,
-          creationParamsCodec: const StandardMessageCodec(),
-        ),
+        child: _Android(creationParams: creationParams),
       );
     } else if (Platform.isIOS && const String.fromEnvironment('IS_MAESTRO_TEST', defaultValue: 'false') != 'true') {
       return SizedBox(
@@ -35,5 +33,47 @@ class CastButton extends StatelessWidget {
       );
     }
     return Container();
+  }
+}
+
+class _Android extends StatelessWidget {
+  const _Android({
+    Key? key,
+    required this.creationParams,
+  }) : super(key: key);
+
+  final Map<String, dynamic> creationParams;
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformViewLink(
+      viewType: 'bccm_player/cast_button',
+      surfaceFactory: (context, controller) {
+        return AndroidViewSurface(
+          controller: controller as AndroidViewController,
+          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+          gestureRecognizers: {
+            Factory<TapGestureRecognizer>(() => TapGestureRecognizer()),
+            Factory<HorizontalDragGestureRecognizer>(() => HorizontalDragGestureRecognizer()),
+          },
+        );
+      },
+      onCreatePlatformView: (params) {
+        return PlatformViewsService.initAndroidView(
+          id: params.id,
+          viewType: 'bccm_player/cast_button',
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: const StandardMessageCodec(),
+          onFocus: () {
+            params.onFocusChanged(true);
+          },
+        )
+          ..addOnPlatformViewCreatedListener((val) {
+            params.onPlatformViewCreated(val);
+          })
+          ..create();
+      },
+    );
   }
 }
